0f5dc1cb1f8ac1c9182ec4851b2238458210daee
yucheng <318771301@qq.com>
2020-01-09 00:16:49 +0800

    添加axios和element ui

--- schoolproject/src/main.js
+++ schoolproject/src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Axios from 'axios'
 
+const axios = Axios.create({
+  baseURL: 'https://localhost:3000',
+  timeout: 1000,
+  // headers: {'X-Custom-Header': 'foobar'}
+});
+Vue.prototype.axios=axios;
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
